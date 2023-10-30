01a25c5deb8003e6e3198bfb4b33d5ab3844f0a5
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2023-10-30 12:20:38 +0800

    Update batchTrans.sol

--- contract/batchTrans.sol
+++ contract/batchTrans.sol
@@ -1,30 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract Distribute {
+    address public owner;
 
-contract Distribute is Ownable {
+    constructor() payable {
+        owner = msg.sender;
+    }
+
     receive() external payable {}
 
-    function handOut(
-        address[] calldata addresses,
-        uint256[] calldata amounts
-    ) public payable onlyOwner() {
-        require(addresses.length > 0, "please enter the acceptance address");
-        for (uint i = 0; i < addresses.length; i++) {
-            payable(addresses[i]).transfer(amounts[i]*(10**14));
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only operator can mint new souls");
+        _;
+    }
+
+    function updateOwner(address newowner) external onlyOwner {
+        require(newowner != address(0), "Invalid Owner");
+        owner = newowner;
+    }
+
+    function handOut(address[] calldata addresses, uint256[] calldata amounts)
+        public
+        payable
+        onlyOwner
+    {
+        require(addresses.length != 0, "please enter the acceptance address");
+        require(amounts.length == addresses.length, "Unmatched length");
+        for (uint256 i = 0; i < addresses.length; i++) {
+            payable(addresses[i]).transfer(amounts[i] * (10**14));
         }
     }
 
-    function withdraw() public onlyOwner() {
+    function withdraw() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-     function getBalance() external view onlyOwner() returns(uint256) {
+    function getBalance() external view returns (uint256) {
         return address(this).balance;
-    }
-
-    function renounceOwnership() public virtual override onlyOwner {
-        revert("renouncing ownership is disabled");
     }
 }
